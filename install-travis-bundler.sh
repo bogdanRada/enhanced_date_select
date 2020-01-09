c5f3db4e8de726ceaa7c7b45bace9594692e5be3
Rada Bogdan <raoul_ice@yahoo.com>
2020-01-09 14:58:58 +0200

    Update install-travis-bundler.sh

--- install-travis-bundler.sh
+++ install-travis-bundler.sh
@@ -6,9 +6,9 @@
 
 rails_version="$(basename "${BUNDLE_GEMFILE:="$(pwd)/Gemfile"}" .gemfile | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')"
 
-echo "Trying to compare rails ${rails_version:="1"} with 5.1"
+echo "Trying to compare rails ${rails_version:="1"} with 5.2"
 
-if (( $(echo "${rails_version%.*} >= 5.1" |bc -l) )); then
+if (( $(echo "${rails_version%.*} >= 5.2" |bc -l) )); then
    echo "Installing Bundler v2"
    gem uninstall -v '< 2.0' -i "$(rvm gemdir)"@global -ax bundler --force || true
    gem install bundler -v '2.0.1'
