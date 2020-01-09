21b4fe5b3d339df1f9e208e183547d8c54b5f7d9
Rada Bogdan <raoul_ice@yahoo.com>
2020-01-09 14:00:40 +0200

    export bundler version after installing on travis

--- install-travis-bundler.sh
+++ install-travis-bundler.sh
@@ -12,10 +12,12 @@ if (( $(echo "${rails_version%.*} >= 5.1" |bc -l) )); then
    echo "Installing Bundler v2"
    gem uninstall -v '< 2.0' -i $(rvm gemdir)@global -ax bundler --force || true
    gem install bundler -v '2.0.1'
+   export MY_BUNDLER_VERSION="_2.0.1_"
 else
    echo "Installing Bundler v1"
    gem uninstall -v '>= 2' -i $(rvm gemdir)@global -ax bundler --force  || true
    gem install bundler -v '1.17.3'
+   export MY_BUNDLER_VERSION="_1.17.3_"
 fi
 
 unset rails_version
